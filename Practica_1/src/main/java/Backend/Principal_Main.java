118ec09ffb3d2ee557b5e08fba4ef7f2d6a21a77
Tu Nombre de JCR2005 <juanrodriguez2022319070@cunoc.edu.gt>
2024-08-04 23:01:20 -0600

    version 0.01, avancen en interfaz grafica

---
+++ Practica_1/src/main/java/Backend/Principal_Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Backend;
+import Fromted.Ventana_Principal;
+
+import javax.swing.*;
+/**
+ *
+ * @author carlosrodriguez
+ */
+
+
+public class Principal_Main {
+    
+    static Ventana_Principal vp = new Ventana_Principal();
+    
+    public static void main(String[] args) {
+        
+        vp.setLocationRelativeTo(null);
+        vp.setVisible(true);
+        
+    }
+    
+}
+
---
+++ Practica_1/src/main/java/Fromted/Imagenes.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Fromted;
+
+import java.awt.Image;
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author carlosrodriguez
+ */
+public class Imagenes {
+    
+    // Ruta de la imagen del fondo 1
+    private static final String logo = "Imagenes/logo.png";
+    
+    public static void logo(JLabel label) {
+        try {
+            URL imageUrl = Imagenes.class.getClassLoader().getResource(logo);
+
+                ImageIcon imagen = new ImageIcon(imageUrl);
+                Image imagenEscalada = imagen.getImage().getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_SMOOTH); // Cambia Image.SCALE_DEFAULT por Image.SCALE_SMOOTH para obtener una mejor calidad
+                ImageIcon iconoEscalado = new ImageIcon(imagenEscalada);
+                label.setIcon(iconoEscalado);
+
+        } catch (Exception e) {
+            System.out.println("Error al cargar la imagen: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
